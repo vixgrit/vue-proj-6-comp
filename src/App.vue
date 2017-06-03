6e47d45226a2e9b4b53ddaabe859d91160e36b74
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-03 17:01:02 -0400

    restructure app folder structure

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,10 @@
 </template>
 
 <script>
-  import Header from './Header.vue';
-  import ServerList from './ServerList.vue'
-  import ServerDetails from './ServerDetails.vue';
-  import Footer from './Footer.vue';
+  import Header from './components/Shared/Header.vue';
+  import ServerList from './components/Server/ServerList.vue'
+  import ServerDetails from './components/Server/ServerDetails.vue';
+  import Footer from './components/Shared/Footer.vue';
   export default {
     components: {
       'my-header': Header,
